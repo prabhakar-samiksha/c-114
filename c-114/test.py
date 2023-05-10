50894f3c5678f6c8922ff6967e932c314c189697
prabhakar-samiksha <103585809+prabhakar-samiksha@users.noreply.github.com>
2023-05-10 19:17:23 +0530

    Add files via upload

---
+++ c-114/test.py
@@ -0,0 +1,13 @@
+a=33
+b=33
+if b>a:
+    print("b is greater tha a")
+elif a==b:
+    print("a and b are equal")
+
+def my_function(fname):
+    print(fname+"Refsnes")
+
+my_function("Email")
+my_function("Tobias")
+my_function("Linus")    
